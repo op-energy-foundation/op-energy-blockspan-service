84ad2b90b06c91222faef9d3983022fca431df22
Dambaev Alexander <ice.redmine@gmail.com>
2024-02-13 18:29:49 +0800

    fix

--- ci-tests.nix
+++ ci-tests.nix
@@ -5,7 +5,7 @@ let
   pkgs = import nixpkgs {};
 
   # Single source of truth for all tests
-  apiPort       = 8995;
+  apiPort       = 8999;
 
   # NixOS module shared between server and client
   sharedModule = {
@@ -14,12 +14,17 @@ let
   };
   op-energy-db-psk-mainnet = "91794224aff99af7f36ee0bb8f210cc99242b15803651fffe68adca52b191416";
   op-energy-db-salt-mainnet = "a9a5fc93af271ae890a28d3cf0f056c8a07e8e6d0326913909707c319fd890d4";
-  bitcoind-signet-rpc-psk = "bcb61e8b0a2c6f998e9996ae1d9da4d650b44a91150d8650be6a9e5c0b67d2d4";
-  bitcoind-signet-rpc-pskhmac = "6cd1a9449750a15c9f6d64b96ee089b9$9f5a5378a4ee0785fae6621c8506168af4b9e2211b2e7a1555012f5d6638744c";
-  op-energy-db-psk-signet = "d1f3c429acfad94eb8e5d31546806e8590946c5f99c90c6e075ddd4d0a57c3f9";
-  op-energy-db-salt-signet = "39c3a80a908523708016a2869841be3077911bc33a90882b7564d0d0eeb000db";
-  op-energy-db-psk-testnet = "5b35acaa6dde71ee103fc74a3fb52e3c51091da78317dfc4fd0a212b77503f99";
-  op-energy-db-salt-testnet = "2069267fa745ecd21a6963c58abf41e4d095719aaa6fff9fca6a9f68088327fb";
+  bitcoind-mainnet-rpc-psk = "bcb61e8b0a2c6f998e9996ae1d9da4d650b44a91150d8650be6a9e5c0b67d2d4";
+  op-energy-account-token-encryption-key = "gzHQ1xTkFevavJ3xxd1fLA4PPxa7rQFXvivEFeEMNfYz99e4WBrywVJEnE/7KGrRYKvkSrSWh5tN/ZsE3cqKj4L57Vhm+hRyoR4oXFxtMVr9Tef2axGSijxQFvp6hocE";
+  bitcoind-mainnet-rpc-pskhmac = "6cd1a9449750a15c9f6d64b96ee089b9$9f5a5378a4ee0785fae6621c8506168af4b9e2211b2e7a1555012f5d6638744c";
+  env = {
+    bitcoind-mainnet-rpc-psk      = bitcoind-mainnet-rpc-psk;
+    bitcoind-mainnet-rpc-pskhmac = bitcoind-mainnet-rpc-pskhmac;
+    op-energy-db-psk-mainnet     = op-energy-db-psk-mainnet;
+    op-energy-db-salt-mainnet    = op-energy-db-salt-mainnet;
+    op-energy-account-token-encryption-key = op-energy-account-token-encryption-key;
+    GIT_COMMIT_HASH              = GIT_COMMIT_HASH;
+  };
 
 in pkgs.nixosTest ({
   # NixOS tests are run inside a virtual machine, and here we specify system of the machine.
@@ -27,23 +32,16 @@ in pkgs.nixosTest ({
 
   nodes = {
     server = args@{ config, pkgs, ... }: let
-      sources = pkgs.copyPathToStore ./op-energy-development;
-      op-energy-host = import ./op-energy-development/host.nix {
-          bitcoind-signet-rpc-pskhmac  = bitcoind-signet-rpc-pskhmac;
-          bitcoind-signet-rpc-psk      = bitcoind-signet-rpc-psk;
-          op-energy-db-psk-mainnet     = op-energy-db-psk-mainnet;
-          op-energy-db-psk-signet      = op-energy-db-psk-signet;
-          op-energy-db-salt-mainnet    = op-energy-db-salt-mainnet;
-          op-energy-db-salt-signet     = op-energy-db-salt-signet;
-          mainnet_node_ssh_tunnel      = false; # disable ssh_tunnel and mainnet service for github action
-          GIT_COMMIT_HASH              = GIT_COMMIT_HASH;
-        };
+      sources = pkgs.copyPathToStore ./op-energy-dev-instance;
+      op-energy-host = import ./op-energy-dev-instance/host.nix env;
+      local_settings = import ./op-energy-dev-instance/local_settings_ci-host.nix env;
     in {
       imports = [
         sharedModule
         op-energy-host
+        local_settings
       ];
-      networking.firewall.allowedTCPPorts = [ 8995 ];
+      networking.firewall.allowedTCPPorts = [ 8999 ];
       networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
 
       users = {
