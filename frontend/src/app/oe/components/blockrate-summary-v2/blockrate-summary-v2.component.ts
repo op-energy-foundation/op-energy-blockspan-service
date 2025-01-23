8384d88f99630cf1a8360e875f1873ec92de3f61
naimish.vora <naimish.vora>
2025-01-23 21:59:47 +0530

    Refactored blockrate summary widget

--- frontend/src/app/oe/components/blockrate-summary-v2/blockrate-summary-v2.component.ts
+++ frontend/src/app/oe/components/blockrate-summary-v2/blockrate-summary-v2.component.ts
@@ -1,46 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  Block,
   BlockTimeStrike,
   BlockTimeStrikePublic,
   PaginationResponse,
 } from '../../interfaces/oe-energy.interface';
-import { BlockTypes, Logos } from '../../types/constant';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Logos } from '../../types/constant';
+import { ActivatedRoute, Router } from '@angular/router';
 import {
   OeBlocktimeApiService,
   OeEnergyApiService,
 } from '../../services/oe-energy.service';
 import { OeStateService } from '../../services/state.service';
 import { ToastrService } from 'ngx-toastr';
-import {
-  catchError,
-  combineLatest,
-  of,
-  Subscription,
-  switchMap,
-  take,
-} from 'rxjs';
-import {
-  getEmptyBlockHeader,
-  getHexValue,
-  toScientificNotation,
-} from '../../utils/helper';
+import { BaseBlockComponent } from '../common/base-block/BaseBlockComponent';
 
 @Component({
   selector: 'app-blockrate-summary-v2',
   templateUrl: './blockrate-summary-v2.component.html',
-  styleUrls: ['./blockrate-summary-v2.component.scss']
+  styleUrls: ['./blockrate-summary-v2.component.scss'],
 })
-export class BlockrateSummaryV2Component implements OnInit {
-
+export class BlockrateSummaryV2Component
+  extends BaseBlockComponent
+  implements OnInit
+{
   logos = Logos;
-  isLoadingBlock = true;
-  subscription: Subscription;
   strike: BlockTimeStrike = {} as BlockTimeStrike;
-  fromBlock: Block;
-  toBlock: Block;
-  latestBlock: Block;
   disabled: boolean = false;
   isSelected: boolean = false;
   selectedGuess: string;
@@ -49,140 +33,51 @@ export class BlockrateSummaryV2Component implements OnInit {
   strikesData = [] as BlockTimeStrikePublic[];
 
   constructor(
-    private router: Router,
+    router: Router,
     private route: ActivatedRoute,
-    private oeEnergyApiService: OeEnergyApiService,
-    private stateService: OeStateService,
-    private oeBlocktimeApiService: OeBlocktimeApiService,
-    private toastr: ToastrService
-  ) {}
+    oeEnergyApiService: OeEnergyApiService,
+    stateService: OeStateService,
+    oeBlocktimeApiService: OeBlocktimeApiService,
+    toastr: ToastrService
+  ) {
+    super(
+      router,
+      oeEnergyApiService,
+      oeBlocktimeApiService,
+      stateService,
+      toastr
+    );
+  }
 
-  ngOnInit() {
-    (this.subscription = this.route.queryParamMap
-      .pipe(
-        switchMap((params: ParamMap) =>
-          this.stateService.latestReceivedBlock$
-            .pipe(take(1)) // don't follow any future update of this object
-            .pipe(
-              switchMap((block: Block) => {
-                this.latestBlock = block;
-                return of(params);
-              })
-            )
-        )
-      )
-      .pipe(
-        switchMap((params: ParamMap) => {
-          const startBlock = params.get('startblock') as string;
-          const endBlock = params.get('endblock') as string;
+  async ngOnInit() {
+    // Initialize the latest block
+    await this.initializeLatestBlock();
 
+    // Process query parameters
+    const params = this.processQueryParams(this.route);
 
-          if (parseInt(startBlock, 10) > this.latestBlock.height) {
-            this.toastr.error('Viewing requires known start block', 'Failed!');
-            return of(null);
-          }
+    // Extract values from processed query parameters
+    const { fromBlockHeight, toBlockHeight } = this.calculateBlockRange({
+      startBlock: +params.startblock,
+      endBlock: +params.endblock,
+    });
 
-          if (parseInt(startBlock, 10) >= parseInt(endBlock, 10)) {
-            this.toastr.error(
-              'Start block must be less than end block',
-              'Failed!'
-            );
-            return of(null);
-          }
-
-          const fromBlockHeight: number =
-            endBlock && startBlock
-              ? parseInt(startBlock, 10)
-              : endBlock
-              ? parseInt(endBlock, 10) - 2016
-              : parseInt(startBlock, 10) || this.latestBlock.height;
-
-          const toBlockHeight: number =
-            parseInt(endBlock, 10) || (startBlock ? +startBlock + 2016 : 1200000);
-
-          // this.fromBlock = undefined;
-          // this.toBlock = undefined;
-
-          this.isLoadingBlock = true;
-
-          return combineLatest([
-            this.oeEnergyApiService
-              .$getBlockByHeight(fromBlockHeight)
-              .pipe(catchError(() => of(getEmptyBlockHeader(fromBlockHeight)))),
-            this.oeEnergyApiService
-              .$getBlockByHeight(toBlockHeight)
-              .pipe(catchError(() => of(getEmptyBlockHeader(toBlockHeight))))
-          ]);
-        })
-      )
-      .subscribe(
-        ([fromBlock, toBlock,]: [
-          Block,
-          Block
-        ]) => {
-          this.fromBlock = fromBlock;
-          if (typeof toBlock === BlockTypes.NUMBER) {
-            this.toBlock = {
-              ...this.fromBlock,
-              height: +toBlock,
-            };
-          } else {
-            this.toBlock = toBlock;
-          }
-          this.isLoadingBlock = false;
-          this.fetchOutcomeNotKnownStrikes();
-        }
-      )),
-      (error) => {
-        this.toastr.error(`Blockspan Failed To Fetch: ${error.error}`, 'Failed!');
+    // Initialize blocks using the base class method
+    this.subscription = this.initializeBlocks(
+      fromBlockHeight,
+      toBlockHeight
+    ).subscribe(
+      ([fromBlock, toBlock]: any) => {
+        this.fromBlock = fromBlock;
+        this.toBlock = toBlock;
         this.isLoadingBlock = false;
-      };
-  }
-
-  getSpan(type: string): string {
-    if (!this.fromBlock || !this.toBlock) return '?';
-
-    if (type === 'blockspan') {
-      return (this.toBlock.height - this.fromBlock.height).toString();
-    }
-
-    if (type === 'time') {
-      return !this.fromBlock.mediantime || !this.toBlock.mediantime
-        ? '?'
-        : (this.toBlock.mediantime - this.fromBlock.mediantime).toString();
-    }
-
-    if (type === 'hashes') {
-      return toScientificNotation(
-        getHexValue(this.toBlock.chainwork) -
-          getHexValue(this.fromBlock.chainwork)
-      );
-    }
-
-    if (type === 'satoshis') {
-      return '?';
-    }
-
-    return '?';
-  }
-
-  getBlockRate(): string {
-    // Ensure fromBlock and toBlock are valid
-    if (!this.fromBlock || !this.toBlock) {
-      return '?';
-    }
-
-    // Retrieve values from getSpan for 'blockspan' and 'time'
-    const blockspan = +this.getSpan('blockspan');
-    const time = +this.getSpan('time');
-
-    // Check if the values are valid numbers and time is not zero to avoid NaN or Infinity
-    if (isNaN(blockspan) || isNaN(time) || time === 0) {
-      return '?'; // Return '?' if the calculation cannot be performed
-    }
-
-    // Perform the calculation and ensure it's valid
-    return ((600 * 100 * blockspan) / time).toFixed(2);
+        this.fetchOutcomeNotKnownStrikes();
+      },
+      (error) => {
+        this.handleError(error);
+        this.isLoadingBlock = false;
+      }
+    );
   }
 
   getResult(): string {
@@ -214,22 +109,6 @@ export class BlockrateSummaryV2Component implements OnInit {
     return this.strike.observedBlockMediantime > this.strike.strikeMediantime;
   }
 
-  goToStrikeDetails(event: Event): string {
-    if (window.getSelection()?.toString()) {
-      // If there is selected text, prevent the click event from propagating
-      event.stopPropagation();
-      return;
-    }
-
-    const queryParams: any = {
-      startblock: this.fromBlock.height,
-      endblock: this.toBlock.height,
-    };
-
-    // Navigate to the target route with the query parameters
-    this.router.navigate(['/hashstrikes/blockspan-details'], { queryParams });
-  }
-
   fetchOutcomeNotKnownStrikes(pageNumber: number = 1): void {
     this.isLoadingBlock = true;
     this.oeBlocktimeApiService
@@ -239,13 +118,9 @@ export class BlockrateSummaryV2Component implements OnInit {
       })
       .subscribe({
         next: (data) => this.handleData(data),
-        error: (error) => this.handleError(error),
+        error: (error) =>
+          this.handleError(`Strikes Failed To Fetch: ${error.error}`),
       });
-  }
-
-  private handleError(error: any): void {
-    this.toastr.error(`Strikes Failed To Fetch: ${error.error}`, 'Failed!');
-    this.isLoadingBlock = false;
   }
 
   private handleData(data: PaginationResponse<BlockTimeStrikePublic>): void {
