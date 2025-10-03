e6b95d081b17d2e4e248da592ee457c1324bfe7a
dambaev <ice.redmine@gmail.com>
2025-10-03 17:11:53 +0800

    Feature/117 blockspans api v2 changes changes (#124)
    * refactor: refactor swagger spec tag in a more generic way
    
    * feat: introduce eitherLogThrowOrReturn and runExceptPrefixT
    
    * feat: introduce verifyPositiveIntEither and verifyPositiveEither in order to be able to handle errors in a pure way
    
    * refactor: refactor getSingleBlockspan to use profile, runExceptPrefixT, eitherLogThrowOrReturn and in terms of Except transformer
    
    ---------
    
    Co-authored-by: Dambaev Alexander <ice.redemine@gmail.com>

--- op-energy-api/src/Data/OpEnergy/API.hs
+++ op-energy-api/src/Data/OpEnergy/API.hs
@@ -9,14 +9,13 @@ module Data.OpEnergy.API where
 import           Control.Lens
 import           Data.Proxy
 import           Data.Swagger
-import qualified Data.HashSet.InsOrd as InsOrd
-import qualified Data.Text as T
 import           Servant.API
 import           Servant.Swagger
 import           Servant.API.WebSocket (WebSocket)
 
 import           Data.OpEnergy.API.V1
 import           Data.OpEnergy.API.V2
+import           Data.OpEnergy.API.Tags
 
 type BackendAPI =
   ( "api" :> "v1" :> V1API {- V1 API -} )
@@ -33,7 +32,7 @@ type SwaggerAPI = "api" :> "v1" :> "swagger.json" :> Get '[JSON] Swagger
 
 -- | API for WebSocket connection. It have to be separate as Websocket does not support servant-client so it should be apart from BackendAPI
 type WebSocketAPI = "api" :> "v1" :> "ws" :> WebSocket
-  :<|> "api" :> "v2" :> "blockspans" :> "ws" :> WebSocket
+  :<|> "api" :> "v2" :> "blockspans" :> "ws" :> Tags "Blockspans" :> WebSocket
 
 -- | This type describes both websocket and backend APIs
 type WebSocketBackendAPI = WebSocketAPI :<|> BackendAPI
@@ -50,23 +49,4 @@ apiSwagger = toSwagger websocketBackendAPI
   & info.version .~ "1.0"
   & info.description ?~ "OpEnergy blockspans service API"
   & info.license ?~ ("MIT" & url ?~ URL "http://mit.com")
-  & paths.itraversed %@~ addBlockSpanTagsToV2Paths
-
-addBlockSpanTagsToV2Paths :: String -> PathItem -> PathItem
-addBlockSpanTagsToV2Paths pathKey pathItem = 
-  if "/api/v2/blockspans/" `T.isInfixOf` T.pack pathKey
-  then addTagToPathItem pathItem
-  else pathItem
-  where
-    addTagToPathItem :: PathItem -> PathItem
-    addTagToPathItem item = item 
-      { _pathItemGet = addTagToOperation <$> _pathItemGet item
-      , _pathItemPost = addTagToOperation <$> _pathItemPost item
-      , _pathItemPut = addTagToOperation <$> _pathItemPut item
-      , _pathItemDelete = addTagToOperation <$> _pathItemDelete item
-      }
-    
-    addTagToOperation :: Operation -> Operation
-    addTagToOperation op = op { _operationTags = InsOrd.singleton "BlockSpan" }
-
 
