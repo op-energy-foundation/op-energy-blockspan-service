187ec3876be94f26c2b0e257ac53f053e522c87e
dambaev <ice.redmine@gmail.com>
2025-12-09 17:36:13 +0000

    Update frontend/module-frontend.nix
    use specific nginx routing

--- frontend/module-frontend.nix
+++ frontend/module-frontend.nix
@@ -241,7 +241,7 @@ in
                   proxy_set_header Connection "Upgrade";
                   limit_conn websocket 100;
           }
-          location /api/v2 {
+          location /api/v2/blockspans {
                   limit_req zone=api burst=10 nodelay;
                   proxy_pass ${cfg.mainnet_api_host}/api/v2;
           }
