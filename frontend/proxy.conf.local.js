022fb284f54a5b04d645374891670c944e673815
naimish.vora <naimish.vora>
2026-02-10 23:33:43 +0530

    reverted package lock changes

--- frontend/proxy.conf.local.js
+++ frontend/proxy.conf.local.js
@@ -1,14 +1,13 @@
 const PROXY_CONFIG = [];
 
-// All APIs go to dev-exchange
-PROXY_CONFIG.push({
-  context: ['/api/**'],
-  target: 'https://dev-exchange.op-energy.info',
-  secure: false,
-  changeOrigin: true,
-  ws: true,
-  proxyTimeout: 30000,
-  logLevel: 'debug',
-});
+PROXY_CONFIG.push(...[
+  {
+    context: ['/api/**'],
+    target: `http://localhost:8999`,
+    secure: false,
+    changeOrigin: true,
+    proxyTimeout: 30000,
+  }
+]);
 
 module.exports = PROXY_CONFIG;
