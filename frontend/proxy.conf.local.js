864c1b1beca7cffadd3508a67cb1b0cc19ae92c9
naimish.vora <naimish.vora>
2026-03-16 21:56:02 +0530

    reverted unwanted changes

--- frontend/proxy.conf.local.js
+++ frontend/proxy.conf.local.js
@@ -2,16 +2,8 @@ const PROXY_CONFIG = [];
 
 PROXY_CONFIG.push(...[
   {
-    context: ['/api/v1/ws'],
-    target: `https://dev-exchange.op.energy`,
-    secure: false,
-    ws: true,
-    changeOrigin: true,
-    proxyTimeout: 30000,
-  },
-  {
     context: ['/api/**'],
-    target: `https://dev-exchange.op.energy`,
+    target: `http://localhost:8999`,
     secure: false,
     changeOrigin: true,
     proxyTimeout: 30000,
