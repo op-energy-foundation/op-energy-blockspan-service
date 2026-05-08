efcea229273b52f40d86c01b728c07117ac740d5
Dambaev Alexander <ice.redemine@gmail.com>
2026-05-08 17:10:52 +0800

    test: refactor: test/OpEnergy/Server/V2/Core/HandleRequest/GenericHandleRequestSpec.hs: move some specs into separate modules

--- op-energy-backend/test/OpEnergy/Server/V2/Core/HandleRequest/GenericHandleRequestSpec.hs
+++ op-energy-backend/test/OpEnergy/Server/V2/Core/HandleRequest/GenericHandleRequestSpec.hs
@@ -38,93 +38,101 @@ import qualified OpEnergy.Server.V2.Environment.DataSource.Request as DataSource
 import qualified OpEnergy.Server.V2.Core.SchedulerThread as Scheduler
 
 spec :: Spec
-spec = describe "handleRequest-generic" <! do
-  tip <- runIO <! QC.generate <! QC.choose (0,10000)
-  blockchain <- runIO <! Bitcoin.generateBlockChain Bitcoin.genesisMediantime 0
-    (verifyNatural tip)
-  (envState, env) <- runIO <! Env.init blockchain
-  let
-      lastblock = blockchain ! tip
-  runIO <! Time.setTimeNS (Env.timeState envState)
-    (fromIntegral (Block.blockHeaderTimestamp lastblock) * 1_000_000_000)
-  shouldContinue <- runIO <! runAppM "test" env <! do
-    Scheduler.iteration
-  it "Scheduler.iteration should not fail" <! do
-    shouldContinue `shouldBe` True
-  let
-      cleanRequestLog = runIO <! STM.atomically <! TVar.writeTVar (Env.requestLogV envState) Seq.Empty
-  cleanRequestLog
-
-
-  it "allPossibleRequests should be > 0" <! do
-    List.length Request.allPossibleRequests `shouldSatisfy` (>0)
-  forM_ Request.allPossibleRequests <! \emptyRequest -> do
-    (requestAllowedToNotToLog, request) <- do
-      case emptyRequest of
-        Request.Ping _ -> return ( True, emptyRequest)
-        Request.GetBlockByHeight _ -> do
-          mheight <- runIO <! do
-            (mheight0 :: Maybe Int) <- QC.generate QC.arbitrary
-            case mheight0 of
-              Nothing -> return Nothing
-              Just _ -> do
-                (Just <. verifyNatural) <$> (QC.generate <! QC.choose (0, tip))
-          return (False, Request.GetBlockByHeight (Call mheight Nothing))
-        -- _ -> False
+spec = do
+  describe "handleRequest-generic" <! do
+    tip <- runIO <! QC.generate <! QC.choose (0,500)
+    blockchain <- runIO <! Bitcoin.generateBlockChain Bitcoin.genesisMediantime 0
+      (verifyNatural tip)
+    (envState, env) <- runIO <! Env.init "handleRequest-generic" blockchain
     let
-        requestT = tshow request |> Text.takeWhile Char.isAlphaNum
+        lastblock = blockchain ! tip
+    runIO <! Time.setTimeNS (Env.timeState envState)
+      (fromIntegral (Block.blockHeaderTimestamp lastblock) * 1_000_000_000)
 
-    it ( Text.unpack requestT <> ": response constructor should always match request") <! do
-      response <- runAppM "test" env <! do
-        HandleRequest.handleRequest request
-      (request == response) `shouldBe'` True <! tshow request
+    it "Scheduler.iteration should not fail" <! do
+      shouldContinue <- runAppM "test" env <! do
+        Scheduler.iteration
+      shouldContinue `shouldBe` True
 
-    it (Text.unpack requestT <> ": any request should profile function at least ones") <! do
-      void <! runAppM "test" env <! do
-        void <! HandleRequest.handleRequest emptyRequest
-      requestLog <- STM.atomically <! TVar.readTVar (Env.requestLogV envState)
+    let
+        cleanRequestLog = runIO <! STM.atomically <! TVar.writeTVar (Env.requestLogV envState) Seq.Empty
+    cleanRequestLog
+
+
+    it "allPossibleRequests should be > 0" <! do
+      List.length Request.allPossibleRequests `shouldSatisfy` (>0)
+    forM_ Request.allPossibleRequests <! \emptyRequest -> do
+      (requestAllowedToNotToLog, request) <- do
+        case emptyRequest of
+          Request.Ping _ -> return ( True, emptyRequest)
+          Request.GetBlockByHeight _ -> do
+            mheight <- runIO <! do
+              (mheight0 :: Maybe Int) <- QC.generate QC.arbitrary
+              case mheight0 of
+                Nothing -> return Nothing
+                Just _ -> do
+                  (Just <. verifyNatural) <$> (QC.generate <! QC.choose (0, tip))
+            return (False, Request.GetBlockByHeight (Call mheight Nothing))
+          -- _ -> False
       let
-          profilesExist = foldl' (\acc logItem ->
-              case logItem of
-                EnvRequest.Profiler (Profiler.Profile (Cast (Just callstack)))
-                  | callstack == requestT -> True
-                _ -> acc
-            )
-            False
-            requestLog
-      profilesExist `shouldBe'` True <! tshow request
+          requestT = tshow request |> Text.takeWhile Char.isAlphaNum
 
-    unless requestAllowedToNotToLog <! do
-      it (Text.unpack requestT <> ": any request should log input at least ones") <! do
+      it ( Text.unpack requestT <> ": response constructor should always match request") <! do
+        response <- runAppM "test" env <! do
+          HandleRequest.handleRequest request
+        (request == response) `shouldBe'` True <! tshow request
+
+      it (Text.unpack requestT <> ": any request should profile function at least ones") <! do
         void <! runAppM "test" env <! do
-          void <! HandleRequest.handleRequest request
+          void <! HandleRequest.handleRequest emptyRequest
         requestLog <- STM.atomically <! TVar.readTVar (Env.requestLogV envState)
         let
-            loggerExist = foldl' (\acc logItem ->
+            profilesExist = foldl' (\acc logItem ->
                 case logItem of
-                  EnvRequest.Logger (Logger.LogInput (Cast (Just _))) -> True
+                  EnvRequest.Profiler (Profiler.Profile (Cast (Just callstack)))
+                    | callstack == "test." <> requestT -> True
                   _ -> acc
               )
               False
               requestLog
-        loggerExist `shouldBe'` True <! tshow request
+        profilesExist `shouldBe'` True <! tshow request
 
-  it "no request should write to data source"
-      <! QC.forAll (Request.generateRequestUpToHeight 0 tip)
-      <! \request-> monadicIO <! do
-    run <! runAppM "test" env <! do
-      void <! HandleRequest.handleRequest request
-    requestLog <- run <! STM.atomically <! TVar.readTVar (Env.requestLogV envState)
-    let
-        dsWritesExist = foldl' (\acc logItem ->
-            case logItem of
-              EnvRequest.DataSource (DataSource.WriteRequest _) -> True
-              _ -> acc
-          )
-          False
-          requestLog
-    when dsWritesExist
-      <! fail <! Text.unpack <! "Failure: " <> tshow request
+      unless requestAllowedToNotToLog <! do
+        it (Text.unpack requestT <> ": any request should log input at least ones") <! do
+          void <! runAppM "test" env <! do
+            void <! HandleRequest.handleRequest request
+          requestLog <- STM.atomically <! TVar.readTVar (Env.requestLogV envState)
+          let
+              loggerExist = foldl' (\acc logItem ->
+                  case logItem of
+                    EnvRequest.Logger (Logger.LogInput (Cast (Just _))) -> True
+                    _ -> acc
+                )
+                False
+                requestLog
+          loggerExist `shouldBe'` True <! tshow request
+
+  describe "handleRequest-generic" <! do
+    tip <- runIO <! QC.generate <! QC.choose (0,500)
+    blockchain <- runIO <! Bitcoin.generateBlockChain Bitcoin.genesisMediantime 0
+      (verifyNatural tip)
+    (envState, env) <- runIO <! Env.init "handleRequest-generic" blockchain
+    it "no request should write to data source"
+        <! QC.forAll (Request.generateRequestUpToHeight 0 tip)
+        <! \request-> monadicIO <! do
+      run <! runAppM "test" env <! do
+        void <! HandleRequest.handleRequest request
+      requestLog <- run <! STM.atomically <! TVar.readTVar (Env.requestLogV envState)
+      let
+          dsWritesExist = foldl' (\acc logItem ->
+              case logItem of
+                EnvRequest.DataSource (DataSource.WriteRequest _) -> True
+                _ -> acc
+            )
+            False
+            requestLog
+      when dsWritesExist
+        <! fail <! Text.unpack <! "Failure: " <> tshow request
 
   where
   shouldBe' expected actual msg = do
