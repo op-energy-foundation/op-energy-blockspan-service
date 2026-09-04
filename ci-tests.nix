9e4895b0ea360510d2ba93abe304645b85e4c4c1
Dambaev Alexander <ice.redemine@gmail.com>
2026-09-04 17:27:09 +0800

    feat: CI VM: provide creds for offer service

--- ci-tests.nix
+++ ci-tests.nix
@@ -16,6 +16,7 @@ let
   op-energy-db-salt-mainnet = "a9a5fc93af271ae890a28d3cf0f056c8a07e8e6d0326913909707c319fd890d4";
   bitcoind-mainnet-rpc-psk = "bcb61e8b0a2c6f998e9996ae1d9da4d650b44a91150d8650be6a9e5c0b67d2d4";
   op-energy-account-token-encryption-key = "gzHQ1xTkFevavJ3xxd1fLA4PPxa7rQFXvivEFeEMNfYz99e4WBrywVJEnE/7KGrRYKvkSrSWh5tN/ZsE3cqKj4L57Vhm+hRyoR4oXFxtMVr9Tef2axGSijxQFvp6hocE";
+  op-energy-internal-service-shared-secret = "mEVAVgAB1LJ1FO93ZL6cdWGp0dnUc9jtQ3zAS785uxjvkV1H7r2GnwTlRUbFGRScQc3QqDun/4X+PkDlci9NboZzCf1TCAEv0tj5GrfpNICytfFlKe5cxaVa13UdR4Ww";
   bitcoind-mainnet-rpc-pskhmac = "6cd1a9449750a15c9f6d64b96ee089b9$9f5a5378a4ee0785fae6621c8506168af4b9e2211b2e7a1555012f5d6638744c";
   env = {
     bitcoind-mainnet-rpc-psk      = bitcoind-mainnet-rpc-psk;
@@ -23,6 +24,7 @@ let
     op-energy-db-psk-mainnet     = op-energy-db-psk-mainnet;
     op-energy-db-salt-mainnet    = op-energy-db-salt-mainnet;
     op-energy-account-token-encryption-key = op-energy-account-token-encryption-key;
+    op-energy-internal-service-shared-secret = op-energy-internal-service-shared-secret;
     GIT_COMMIT_HASH              = GIT_COMMIT_HASH;
   };
 
